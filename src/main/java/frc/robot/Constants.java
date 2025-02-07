30e86e05ad5d5f5468696534398183a5a4111344
TeamThundercats2657 <164930731+TeamThundercats2657@users.noreply.github.com>
2025-02-06 17:59:34 -0700

    fixed angle encoders had to invert

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,7 @@ public final class Constants
   public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(112);
+  public static final double MAX_SPEED  = Units.feetToMeters(100);
   // Maximum speed of the robot in meters per second, used to limit acceleration.
 
 //  public static final class AutonConstants
@@ -48,4 +48,20 @@ public final class Constants
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT    = 6;
   }
+  public static class AlgaeConstants {
+    // PWM ports/CAN IDs for motor controllers
+    public static final int kAmpShooterID = 12;
+
+    // Current limit for launcher and feed wheels
+    public static final int kAmpCurrentLimit = 80;
+
+    // Speeds for wheels when intaking and launching for the Amp. Intake speeds are negative to run the wheels
+    // in reverse
+    public static final double kAmpIntakeSpeed = .2;
+    public static final double kAmpShot = -.5;
+
+    public static final double kAmpDelay = 1;
+
+    public static final double kWheelDiameterInches = 6;
+}
 }
